b624b6e74824cde00f35ad0b5c3cb683b25650b2
diablo512 <diablo512x@gmail.com>
2010-03-13 16:17:50 +0100

    administration panel

---
+++ views/admin/index.php
@@ -0,0 +1,29 @@
+<h3>Moderators</h3>
+<?php foreach( $moderators as $moderator ):?>
+ <li>
+<b><?php echo $moderator['name'];?></li>
+ <?php endforeach;?>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ <?php  /*
+ <h1><?php echo $count;?> Tags</h1>
+
+<div style="clear:both"></div>
+
+<div class="tags_list">
+<ul class="holder noborder">
+<?php foreach ($tags as $tag):?>
+<li class="bit-box nopadding"><a href="<?php echo basePath();?>/questions?tag=<?php echo $tag['tag'];?>"><?php echo $tag['tag'];?></a> x <?php echo $tag['count'];?></li>
+<?php endforeach;?>
+</ul>
+</div>
+
+*/?>
