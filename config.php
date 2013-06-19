3b68aa2f057443dff9f1fa3d086ddcb508257b62
Ifeoluwa Okpah <ifewalter@gmail.com>
2013-06-19 06:17:03 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 // Database Details
 define('SERVERNAME','localhost');
-define('SERVERPORT','3309');
-define('DBUSERNAME','root');
-define('DBPASSWORD','ifewalter');
-define('DBNAME','qwench');
+define('SERVERPORT','3306');
+define('DBUSERNAME','your_db_username_here');
+define('DBPASSWORD','your_db_password_here');
+define('DBNAME','your_db_name_here');
 
 Define('ANSWERS_PER_PAGE','10');
 define('QUESTIONS_PER_PAGE','10');
